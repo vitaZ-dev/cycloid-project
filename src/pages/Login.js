461e424826811e98c8952f02570c4dc594d9883d
vitaZ-dev <vitaminzdev@gmail.com>
2024-02-02 18:39:28 +0900

    feat: 로그인 페이지 구조 세팅

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,21 @@
 import react from "react";
+import { LoginWrap } from "../style/LoginCSS";
+import logo from "../images/icon_main.png"
 
 const Login = () => {
   return (
-    <>
-      로그인페이지
-    </>
+    <LoginWrap id="login">
+      <img src={logo} alt="logo" />
+      <h1>통합로그인</h1>
+      <div>
+        로그인 폼
+      </div>
+    </LoginWrap>
   )
 }
 
 export default Login;
+
+/*
+rgb(3,179,233)
+*/
