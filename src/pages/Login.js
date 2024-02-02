d45bce78452e37e0390e2d06c4e30f0bffb5930e
vitaZ-dev <vitaminzdev@gmail.com>
2024-02-02 21:13:52 +0900

    feat: 로그인페이지 구현 - 퍼블리싱 및 페이지 전환 기능 추가

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,21 +1,62 @@
 import react from "react";
-import { LoginWrap } from "../style/LoginCSS";
+import { LoginForm, LoginWrap } from "../style/LoginCSS";
 import logo from "../images/icon_main.png"
+import { Checkbox, Form, Input } from "antd";
+import { Link, useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const nav = useNavigate();
+  const onFinish = (values) => {
+    console.log("Received values of form: ", values);
+    nav("/");
+  };
+
   return (
     <LoginWrap id="login">
       <img src={logo} alt="logo" />
       <h1>통합로그인</h1>
-      <div>
-        로그인 폼
-      </div>
+      <LoginForm>
+        <Form
+          name="normal_login"
+          className="login-form"
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+        >
+          <Form.Item
+            name="아이디"
+            rules={[
+              {
+                required: true,
+                message: "아이디를 입력해주세요",
+              },
+            ]}
+          >
+            <Input placeholder="아이디" />
+          </Form.Item>
+          <Form.Item
+            name="비밀번호"
+            rules={[
+              {
+                required: true,
+                message: "비밀번호를 입력해주세요",
+              },
+            ]}
+          >
+            <Input type="password" placeholder="비밀번호" />
+          </Form.Item>
+
+          <div className="another-check">
+            <Form.Item name="remember" valuePropName="checked" noStyle>
+              <Checkbox>아이디 저장</Checkbox>
+            </Form.Item>
+            <Link to="/signup" className="login-form-forgot">회원가입</Link>
+          </div>
+
+          <button type="submit">로그인</button>
+        </Form>
+      </LoginForm>
     </LoginWrap>
   )
 }
 
 export default Login;
-
-/*
-rgb(3,179,233)
-*/
--- src/style/LoginCSS.js
+++ src/style/LoginCSS.js
@@ -1,3 +1,50 @@
 import styled from '@emotion/styled';
 
-export const LoginWrap = styled.section``;
+export const LoginWrap = styled.section`
+  width: 420px;
+  height: 100vh;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  
+  img {
+    width: 120px;
+    height: 120px;
+  }
+
+  h1 {
+    font-size: 24px;
+    &::before {
+      content: 'CYCLOID';
+      display: block;
+      font-size: 14px;
+      font-weight: 500;
+      text-align: center;
+    }
+  }
+`;
+
+export const LoginForm = styled.div`
+  width: 80%;
+  margin-top: 36px;
+  .ant-form-item-explain-error {
+    padding-left: 8px;
+    font-size: 12px;
+  }
+  .another-check {
+    margin-bottom: 24px;
+    display: flex;
+    justify-content: space-between;
+  }
+  button[type=submit] {
+    width: 100%;
+    height: 40px;
+    border-radius: 5px;
+    background: #03b3e9;
+    font-size: 14px;
+    color: #f9f9f9;
+    font-weight: 500;
+  }
+`;
