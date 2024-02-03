6cdb3de18d5ff43cb93a6630adf2bfe387b21785
vitaZ-dev <vitaminzdev@gmail.com>
2024-02-03 15:44:56 +0900

    feat: 회원가입 페이지 작업 1차 완료 - 퍼블리싱 완료 - 정규표현식으로 입력값 검증 - 정보 입력 후 로그인 페이지로 전환

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,9 +1,11 @@
-import react, { useState } from "react";
+import react, { useEffect, useState } from "react";
 import CommonInput from "../components/CommonInput";
 import { SignUpWrap } from "../style/SingUpCSS";
 import { Checkbox } from 'antd';
+import { useNavigate } from "react-router-dom";
 
 const SignUp = () => {
+  const nav = useNavigate();
   const [userId, setUserId] = useState("");
   const [userPhone, setUserPhone] = useState("");
   const [userPw, setUserPw] = useState("");
@@ -34,49 +36,90 @@ const SignUp = () => {
       "contents": "이메일, SMS, 알림수신동의 (선택)"
     },
   ]
-  const onChange = (e) => {
-    const checked = e.target.checked;
-    console.log(`checked = ${checked}`);
-    // setValue(checked);
+  const submitSignUp = () => {
+    if (!(check01&&check02&&check03)) {
+      alert("필수 약관에 동의해주세요");
+      return;
+    }
+    const blankPattern = /^\s*$/;
+    if (blankPattern.test(userId)) {
+      alert("아이디를 입력해주세요");
+      return;
+    }
+    if (blankPattern.test(userPw)) {
+      alert("비밀번호를 입력해주세요");
+      return;
+    }
+    const phonePattern = /^010\d{8}$/;
+    if (!phonePattern.test(userPhone)) {
+      alert("올바른 형식의 전화번호를 입력해주세요")
+      return;
+    }
+    // check04 ? console.log("선택 항목 체크") : "";
+    const userInfo = {
+      "id": userId,
+      "password": userPw,
+      "phone": userPhone,
+      "agreement": true,
+      "ad_agree": check04,
+    }
+    console.log(userInfo);
+    alert("회원가입이 완료되었습니다")
+    nav("/login");
   };
+
+  const [disable, setDisable] = useState(true);
+  useEffect(() => {
+    const blankPattern = /^\s*$/;
+    const idCheck = !blankPattern.test(userId);
+    const phoneCheck = !blankPattern.test(userPhone);
+    const pwCheck = !blankPattern.test(userPw);
+    const infoCheck = idCheck&&phoneCheck&&pwCheck;
+
+    infoCheck && (check01&&check02&&check03) 
+      ? setDisable(false) : setDisable(true);
+  }, [userId, userPhone, userPw, check01, check02, check03])
+
   return (
     <SignUpWrap>
-      <CommonInput
-        title="ID"
-        type="text"
-        value={userId}
-        setValue={setUserId}
-        placeholder="홍길동"
-      />
-      <CommonInput
-        title="휴대폰 번호"
-        type="text"
-        value={userPhone}
-        setValue={setUserPhone}
-        placeholder="01000000000"
-      />
-      <CommonInput
-        title="비밀번호"
-        type="password"
-        value={userPw}
-        setValue={setUserPw}
-        placeholder="비밀번호"
-      />
+      <div className="signup-form">
+        <CommonInput
+          title="ID"
+          type="text"
+          value={userId}
+          setValue={setUserId}
+          placeholder="홍길동"
+        />
+        <CommonInput
+          title="휴대폰 번호"
+          type="text"
+          value={userPhone}
+          setValue={setUserPhone}
+          placeholder="01000000000"
+        />
+        <CommonInput
+          title="비밀번호"
+          type="password"
+          value={userPw}
+          setValue={setUserPw}
+          placeholder="비밀번호"
+        />
 
-      <p>필수 약관 동의</p>
-      {checkBoxItem.map((item, idx) => (
-        <div className="check-box" key={idx}>
-          <Checkbox
-            checked={item.check}
-            onChange={(e) => item.setCheck(e.target.checked)}
-          >
-            {item.contents}
-          </Checkbox>
-          { idx !== 3 ? <span className="show-detail">전문보기</span> : null}
-        </div>
-      ))}
+        <p>필수 약관 동의</p>
+        {checkBoxItem.map((item, idx) => (
+          <div className="check-box" key={idx}>
+            <Checkbox
+              checked={item.check}
+              onChange={(e) => item.setCheck(e.target.checked)}
+            >
+              {item.contents}
+            </Checkbox>
+            { idx !== 3 ? <span className="show-detail">전문보기</span> : null}
+          </div>
+        ))}
+      </div>
 
-      <button>회원가입 완료하기</button>
+      <button disabled={disable} onClick={submitSignUp}>회원가입 완료하기</button>
     </SignUpWrap>
   )
 }
--- src/style/SingUpCSS.js
+++ src/style/SingUpCSS.js
@@ -3,24 +3,44 @@ import { baseContainer } from './CommonStyle';
 
 export const SignUpWrap = styled.section`
   ${baseContainer};
-  p {
+  justify-content: space-around;
+  .signup-form {
     width: 100%;
-    margin-bottom: 5px;
-    font-size: 16px;
-    font-weight: 700;
-    text-align: left;
-  }
-  .check-box {
-    width: 100%;
-    padding: 4px;
-    display: flex;
-    justify-content: space-between;
-    .show-detail {
-      display: block;
-      color: #bbb;
-      line-height: 22px;
-      text-decoration: underline;
-      cursor: pointer;
+    p {
+      width: 100%;
+      margin-bottom: 5px;
+      font-size: 16px;
+      font-weight: 700;
+      text-align: left;
     }
+    .check-box {
+      width: 100%;
+      padding: 4px;
+      display: flex;
+      justify-content: space-between;
+      .show-detail {
+        display: block;
+        color: #bbb;
+        line-height: 22px;
+        text-decoration: underline;
+        cursor: pointer;
+      }
+    }
+  }
+
+  button {
+    width: 100%;
+    height: 40px;
+    border-radius: 5px;
+    background-color: #03b3e9;
+    font-size: 14px;
+    color: #f9f9f9;
+    font-weight: 500;
+      &:disabled,
+      &[disabled] {
+        background-color: #d8d8d8;
+        color: #888;
+        cursor: not-allowed;
+      }
   }
 `;
